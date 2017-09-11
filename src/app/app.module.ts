c372da1dba0b9b7f81429391b439df2e76e12e26
oscarjorge <oscarjorgecastillo@gmail.com>
2017-09-11 22:52:57 +0200

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
@@ -56,6 +56,7 @@ export const firebaseConfig = {
     BrowserModule,
     HttpModule,
     FormsModule,
+    ReactiveFormsModule,
     APP_ROUTING,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
--- src/app/components/jugadores/edicion.component.ts
+++ src/app/components/jugadores/edicion.component.ts
@@ -1,6 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from "@angular/forms";
+import {  FormGroup, FormArray, FormControl, Validators } from "@angular/forms";
 import { Router, ActivatedRoute } from "@angular/router";
 import { Jugador } from "../../interfaces/jugador.interface";
 import { JugadoresService } from "../../services/jugadores.service";
@@ -12,8 +12,9 @@ import { EquiposService } from "../../services/equipos.service";
   templateUrl: './edicion.component.html',
   styles: []
 })
-export class EdicionJugadorComponent implements OnInit {
+export class EdicionJugadorComponent {
 
+  forma:FormGroup;
   private jugador: Jugador = {
     Nombre: "",
     Caracteristicas: "",
@@ -36,7 +37,22 @@ export class EdicionJugadorComponent implements OnInit {
         if (this.id !== "nuevo") {
           this._jugadoresService.getJugador(this.id).then(data => {
             this.jugador = <Jugador>data;
-           
+            let arrEquipos = [];
+            this.jugador.Equipos.forEach(element => {
+              arrEquipos.push(new FormControl(element.Key));
+            });
+            console.log(arrEquipos)
+            this.forma= new FormGroup({
+              'nombre': new FormControl(this.jugador.Nombre, [Validators.required, Validators.minLength(3)]),
+              'apodo': new FormControl(this.jugador.Apodo),
+              'imagen': new FormControl(this.jugador.Imagen),
+              'caracteristicas': new FormControl(this.jugador.Caracteristicas),
+              'equipos': new FormArray(arrEquipos)
+            })
+
+            console.log(this.jugador)
+
+
             _equiposService.getEquipos().subscribe(data => {
               this.equipos=data;
 
@@ -57,70 +73,77 @@ export class EdicionJugadorComponent implements OnInit {
         }
         
       });
+      this.forma= new FormGroup({
+        'nombre': new FormControl(),
+        'caracteristicas': new FormControl(),
+        'imagen': new FormControl(),
+        'apodo': new FormControl(),
+        'equipos': new FormArray([new FormControl('aa')])
+      })
+  }
+  agregarEquipo(equipo){
+    (<FormArray>this.forma.controls['equipos']).push(new FormControl(equipo));
   }
 
-  ngOnInit() {
-    // console.log(this.jugador);
-    // console.log(this.jugador.Equipos.filter(eq=>eq.$key==1))
-  }
-
-  guardar(forma: NgForm) {
-    console.log(forma)
-    let equiposFiltrados=[];
-    for (var property in forma.controls) {
-      if (forma.controls.hasOwnProperty(property)) {
-        if (property.indexOf('equ_') >= 0)
-          if (forma.controls[property].value == true) {
-            let filtro = this.equipos.filter(eq => eq.Nombre == property.substring(property.indexOf('equ_') + 4, property.length));
-            equiposFiltrados.push(filtro[0]);
-          }
-      }
-    }
-    if (equiposFiltrados != null && equiposFiltrados.length > 0) {
-      this.jugador.Equipos = [];
-      equiposFiltrados.forEach(equipo => {
-        this.jugador.Equipos.push({
-          'Key': equipo.$key
-        })
-      });
+  guardar() {
+    console.log(this.forma.value)
+    console.log(this.forma)
+    // console.log(forma)
+    // let equiposFiltrados=[];
+    // for (var property in forma.controls) {
+    //   if (forma.controls.hasOwnProperty(property)) {
+    //     if (property.indexOf('equ_') >= 0)
+    //       if (forma.controls[property].value == true) {
+    //         let filtro = this.equipos.filter(eq => eq.Nombre == property.substring(property.indexOf('equ_') + 4, property.length));
+    //         equiposFiltrados.push(filtro[0]);
+    //       }
+    //   }
+    // }
+    // if (equiposFiltrados != null && equiposFiltrados.length > 0) {
+    //   this.jugador.Equipos = [];
+    //   equiposFiltrados.forEach(equipo => {
+    //     this.jugador.Equipos.push({
+    //       'Key': equipo.$key
+    //     })
+    //   });
       
-      if (this.id == "nuevo") {
-        // insertando
-        this._jugadoresService.nuevoJugador(this.jugador)
-          .then(res => {
-            this.router.navigate(['/jugador', res.key])
-          })
-          .catch((error) => {
-            console.error(error);
-            alert("Se ha producido un error.")
-          })
+    //   if (this.id == "nuevo") {
+    //     // insertando
+    //     this._jugadoresService.nuevoJugador(this.jugador)
+    //       .then(res => {
+    //         this.router.navigate(['/jugador', res.key])
+    //       })
+    //       .catch((error) => {
+    //         console.error(error);
+    //         alert("Se ha producido un error.")
+    //       })
 
-      } else {
-        //actualizando
-        this._jugadoresService.actualizarJugador(this.jugador, this.id)
-          .then(data => {
-            this.router.navigate(['/jugadores'])
-          },
-          error => console.error(error))
-          .catch(() => { alert("Se ha producido un error.") });
-      }
-    }
-    else {
-      alert("El jugador debe pertenecer por los menos a un quipo");
-    }
+    //   } else {
+    //     //actualizando
+    //     this._jugadoresService.actualizarJugador(this.jugador, this.id)
+    //       .then(data => {
+    //         this.router.navigate(['/jugadores'])
+    //       },
+    //       error => console.error(error))
+    //       .catch(() => { alert("Se ha producido un error.") });
+    //   }
+    // }
+    // else {
+    //   alert("El jugador debe pertenecer por los menos a un quipo");
+    // }
 
 
-
-  }
-
-  agregarNuevo(forma: NgForm) {
-
-    this.router.navigate(['/editarJugador', 'nuevo']);
-
-    forma.reset({
-      equipo: "Vividores"
-    });
 
   }
+
+  // agregarNuevo(forma: NgForm) {
+
+  //   this.router.navigate(['/editarJugador', 'nuevo']);
+
+  //   forma.reset({
+  //     equipo: "Vividores"
+  //   });
+
+  // }
 
 }
